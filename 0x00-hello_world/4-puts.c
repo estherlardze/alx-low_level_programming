85068c31deec657c2ffac3f3bd5726da89f68e17
EstherLard <estherlardze8@gmail.com>
2022-03-12 13:50:47 -0800

    Printing a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - Printing a string folowed by new line
+ * Return: 0 if success.
+ */
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
