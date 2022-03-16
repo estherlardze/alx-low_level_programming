1a30f2bfc67f13796b5419a6977aff47baf15f86
EstherLard <estherlardze8@gmail.com>
2022-03-15 17:03:22 -0700

    print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,13 +3,14 @@
 /**
  * main - check the code
  *
- * Return: always 0
- * */
+ * Return : always 0
+ **/
+
 int main(void)
 {
-  print_alphabet();
- 
-  return (0);
+int _putchar(char c);
+
+return (0);
 
 
 }
