67e2044b56f4a8465c1d1eeacc25247f97c9a5eb
Esther <99372812+EstherLard@users.noreply.github.com>
2022-03-17 20:17:43 +0000

    Returning upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,15 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - uppercase letters
+ * _isupper - uppercase letters
  * @c: character to check
  *
- * Return: 1 or 0
-*/
+ * Return: 0 or 1
+ */
 
-int _isupper(int c);
+
+int _isupper(int c)
 {
-
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
