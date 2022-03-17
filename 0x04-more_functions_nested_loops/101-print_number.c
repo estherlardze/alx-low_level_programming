87207c0e13e6338841581bbafbd0543afe504beb
EstherLard <estherlardze8@gmail.com>
2022-03-17 15:23:39 -0700

    printing an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_integer - print function that print integer
+ * @n: printing n
+ *
+ * Return: void
+ */
+
+void print_number(int n);
+{
+	_putchar('98');
+
+	return (0);
+
+}
