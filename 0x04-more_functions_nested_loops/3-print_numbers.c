198d005a1bd4fe2524ee431b4ca44aec1ca2c03a
EstherLard <estherlardze8@gmail.com>
2022-03-17 13:51:58 -0700

    printing from 1 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number fron 1 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+
+}
