d4cbc6e1d69fd17842c565fe2560e74421433739
EstherLard <estherlardze8@gmail.com>
2022-03-21 15:40:48 -0700

    printing lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen -prints lenght of string
+ * @s: pointer to the string to check
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	for (int i = 0; i <= s[i]; i++)
+
+	return (i);
+
+
+}
