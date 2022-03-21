2a682d6fdbce2df9afb109c5aea9471cefa0154f
EstherLard <estherlardze8@gmail.com>
2022-03-21 16:11:09 -0700

    print sting in reversse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: pointer to string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+
+}
