e3aa1662f6666e6d362b17a72dde942682aed321
Esther <99372812+EstherLard@users.noreply.github.com>
2022-03-21 21:50:49 +0000

    changes to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,14 @@
+
 #include "main.h"
-
 /**
- * reset_to_98 - update n to 98
+ * reset_to_98 - reset argument to 98
  * @n: pointer
- * Return: void.
- */
-void reset_to_98(int *n);
+ * Return: void
+*/
+
+void reset_to_98(int *n)
 {
-	*n = 98;
 
-
-
-
-
+*n = 98;
 
 }
