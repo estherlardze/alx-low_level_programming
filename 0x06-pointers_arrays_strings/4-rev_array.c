6e016a920d74f0102d247c3080e544294bfc901e
EstherLard <estherlardze8@gmail.com>
2022-03-24 16:02:01 -0700

    reversing a string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse
+ * @a : pointer to int params
+ * @n : pointer to int params
+ * Return: no
+ */
+
+void reverse _array(int *a, int n);
+{
+	char n[10] = "Bismark";
+
+	char a = strrev(n)
+
+	printf("%s", a);
+
+
+}
