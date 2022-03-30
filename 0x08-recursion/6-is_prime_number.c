db05d562bf67c92023bd3811606cdf3b734e57da
EstherLard <estherlardze8@gmail.com>
2022-03-30 08:30:02 -0700

    prinme number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * is_prime_number - check if n is a prime number
+ * @n: int
+ * Return: 0 or 1
+ */
+
+
+int is_prime_number(int n)
+{
+return (check_prime(n, 2));
+}
+
+int check_prime(int n, int resp)
+{
+
+if (resp >= n && n > 1)
+	return (1);
+else if (n % resp == 0 || n <= 1)
+	return (0);
+else
+	return (check_prime(n, resp + 1));
+}
