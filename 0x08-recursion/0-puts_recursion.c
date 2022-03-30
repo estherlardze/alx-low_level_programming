9f6d41d6bb100a645ebaf4480270bde62c3d73cf
EstherLard <estherlardze8@gmail.com>
2022-03-30 07:49:57 -0700

    void _puts_recursion(char *s)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,18 +3,20 @@
 /**
  * _puts_recursion - print a string.
  * @s: pointer to char
- * Return: No.
+ * Return: void.
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
+
+if (*s == '\0')
+{
 	_putchar('\n');
-	}
+}
+else
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
 }
