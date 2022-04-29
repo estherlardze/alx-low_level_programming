6f8993da10cd7bd297313f65bbab2e5af5ba3ba0
Esther <99372812+EstherLard@users.noreply.github.com>
2022-04-29 22:34:14 +0000

    Read a text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,36 @@
 #include "main.h"
-
 /**
- * read_textfile - a function that reads a
- * text file and prints it to the POSIX standard output
- * @filename: of the file
- * @letters: the number of letters it should read and print
- * Return: the actual number of letters it could read and print
+ * read_textfile - function to read and write a file
+ * @filename: const char type pointer to file to be read
+ * @letters: size_t type
+ * Return: always successful
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	register int _open, _read, _write;
-	register char *buffer = malloc(sizeof(char)  * letters);
+	int fd;
+	ssize_t fdread, fdwrite, fdclose;
+	char *space;
 
-	if (!(buffer))
-	{
-		free(buffer);
+	if (filename == NULL)
 		return (0);
-	}
-	if (!(filename))
+	space = malloc(sizeof(char) * letters);
+	if (space == NULL)
 	{
-		return (0);
+		return (-1);
 	}
-	_open = open(filename, O_RDONLY);
-	_read = read(_open, buffer, letters);
-	_write = write(STDOUT_FILENO, buffer, _read);
 
-	if (_open == -1 || _read == -1 || _write == -1 || !(_write == _read))
-	{
-		free(buffer);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	}
-	free(buffer);
-	close(_open);
-	return (_write);
+	fdread = read(fd, space, letters);
+	if (fdread == -1)
+		return (-1);
+	fdwrite = write(STDOUT_FILENO, space, fdread);
+
+	if (fdwrite == -1)
+		return (-1);
+	fdclose = close(fd);
+	if (fdclose == -1)
+		return (-1);
+	return (fdread);
 }
