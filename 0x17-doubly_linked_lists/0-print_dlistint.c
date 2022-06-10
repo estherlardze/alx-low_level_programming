5538ff6d30e5b9a14689d41a56bd654971b7e0f5
Esther <99372812+EstherLard@users.noreply.github.com>
2022-06-10 04:18:59 +0000

    Print element of double link

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function to print the elements of a doubly linked list
+ * @h: pointer of const dlistint_t type
+ * Return: return list and number of elements
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
