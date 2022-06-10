cde44b4a273f7acc597420834c663c9f65fc120d
Esther <99372812+EstherLard@users.noreply.github.com>
2022-06-10 04:26:30 +0000

    print doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	unsigned int counter = 0;
+	unsigned int count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		counter++;
+		count++;
 	}
-	return (counter);
+	return (count);
+}
