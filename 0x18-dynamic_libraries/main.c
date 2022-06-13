0b4bd60abe69dbc42b103a727896df4bbfb07887
Esther <99372812+EstherLard@users.noreply.github.com>
2022-06-13 22:56:14 +0000

    Dynamic library

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -8,5 +8,6 @@
  */
 int main(void)
 {
-    printf("%d\n", _strlen("main"));
-    return (EXIT_SUCCESS);
+	printf("%d\n", _strlen("Holberton"));
+	return (EXIT_SUCCESS);
+}
